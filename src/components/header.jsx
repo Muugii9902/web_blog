80fa2d1fd5582fea288482a0c27ee405ce607883
Muugii9902 <muugii9902@gmail.com>
2024-08-01 12:30:49 +0800

    web_blog

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="w-[1917px] h-[100px]">
+      <div>
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Header;
