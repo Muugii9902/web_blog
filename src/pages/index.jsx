b9c686f4d4c2ae415a0dfd8091c77ba148685bd7
Muugii9902 <muugii9902@gmail.com>
2024-08-06 12:45:43 +0800

    webblog

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 import Content from "@/components/content/content";
-import AllBlogpost from "@/components/allblogpost";
+import AllBlogpost from "@/pages/allblogpost";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +10,6 @@ export default function Home() {
   return (
     <main>
       <Content />
-      <AllBlogpost />
     </main>
   );
 }
