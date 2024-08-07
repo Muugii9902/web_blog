a06fd809749ff729ff4e5c3fad2568d886615655
Muugii9902 <muugii9902@gmail.com>
2024-08-07 12:16:24 +0800

    web blog

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Menu from "./menu";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <section className="container m-auto h-[100px] flex justify-between">
       <div className="container  h-[36px] last:flex justify-between m-10">
         <img className="w[158px] h-[36px]" src="/images/logo.png" alt="" />
-        <Menu />
+        <ul className="flex gap-10">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contactus">Contact</Link>
+          </li>
+        </ul>
         <input
           className="border border-stone-400 rounded-md"
           type="Search"
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import Content from "@/components/content/content";
 import AllBlogpost from "@/pages/allblogpost";
@@ -7,6 +9,22 @@ import AllBlogpost from "@/pages/allblogpost";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  const getArticleData = async () => {
+    const response = await fetch(
+      "https://dev.to/api/articles?page=1&perpage=9"
+    );
+    const data = await response.json();
+    setArticles(data);
+    // console.log("data", data)
+  };
+
+  useEffect(() => {
+    getArticleData();
+  }, []);
+  console.log("article", articles);
+
   return (
     <main>
       <Content />
