85e8c6afec5be1408d10e16ab2083ec946b560d4
Muugii9902 <muugii9902@gmail.com>
2024-08-15 12:32:59 +0800

    duusan

--- src/components/content/trendingcard.jsx
+++ src/components/content/trendingcard.jsx
@@ -7,7 +7,7 @@ const TrendingCard = ({ image, type, text }) => {
             className="rounded-xl h-[320px] w-[290px] bg-cover bg-blend-darken  bg-[rgba(0,0,0,0.5)]"
             style={{ backgroundImage: `url(${image})` }}
           >
-            <div className=" ">
+            <div className="">
               <button className="py-1 px-2.5 rounded-lg bg-[#4B6BFB] text-white mb-4 font-medium">
                 {type}
               </button>
